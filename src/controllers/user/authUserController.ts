e09067ef567baee76308996188b358c144ebc8f3
renelps <renanlps616@gmail.com>
2025-06-20 09:09:42 -0300

    feat(user): add user controllers (auth, create, detail)

---
+++ src/controllers/user/authUserController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { AuthUserService } from "../../services/user/authUserService";
+
+class AuthUserController {
+  async handle(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const authUserService = await new AuthUserService();
+
+    const auth = await authUserService.execute({ email, password})
+
+    return res.json(auth)
+  }
+}
+
+export { AuthUserController }
